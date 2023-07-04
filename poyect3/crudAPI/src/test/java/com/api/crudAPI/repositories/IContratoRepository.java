fbb47dde1d56ba2cfefca0b6e4781f734100efcc
Paola Arrieta <116530227+Paola-AV@users.noreply.github.com>
2023-07-04 16:10:14 -0600

    Delete poyect3/crudAPI directory

--- poyect3/crudAPI/src/test/java/com/api/crudAPI/repositories/IContratoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.crudAPI.repositories;
-
-import com.api.crudAPI.models.ContratoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface IContratoRepository extends JpaRepository<ContratoModel, Long>  {
-    
-}
