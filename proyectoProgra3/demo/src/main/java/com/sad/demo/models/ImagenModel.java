f28228db5742264e893fa96d954941efed9f61c9
Paola Arrieta <116530227+Paola-AV@users.noreply.github.com>
2023-07-24 11:41:28 -0600

    Add files via upload

---
+++ proyectoProgra3/demo/src/main/java/com/sad/demo/models/ImagenModel.java
@@ -0,0 +1,48 @@
+package com.sad.demo.models;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // denota que es una entidad
+@Table(name = "imagen") // da el nombre a la tabla en mysql
+
+public class ImagenModel {
+    @Id // se genera el ID de la tabla
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // el id tiene un auto_increment en la base de datos
+    private Long IDimagen;
+
+    // demas columnas de la tabla
+    @Column
+    private String imagen;
+    @Column
+    private int IDpropiedad;
+
+    // get and set de cada columna
+    public Long getIDimagen() {
+        return this.IDimagen;
+    }
+
+    public void setIDimagen(Long IDimagen) {
+        this.IDimagen = IDimagen;
+    }
+
+    public String getImagen() {
+        return this.imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public int getIDpropiedad() {
+        return this.IDpropiedad;
+    }
+
+    public void setIDpropiedad(int IDpropiedad) {
+        this.IDpropiedad = IDpropiedad;
+    }
+
+}
