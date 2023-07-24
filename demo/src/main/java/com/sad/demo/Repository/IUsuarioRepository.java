57119c2c7ee95e4a5ac0d704fe0139c36a791382
Paola Arrieta <116530227+Paola-AV@users.noreply.github.com>
2023-07-24 11:34:28 -0600

    Delete demo directory

--- demo/src/main/java/com/sad/demo/Repository/IUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sad.demo.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sad.demo.models.UsuarioModel;
-
-
-public interface IUsuarioRepository extends JpaRepository<UsuarioModel, Long> {
-    
-}
